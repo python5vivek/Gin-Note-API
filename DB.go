d4197c9154703128275751af43b228f5e308c5e0
python5vivek <godlevel.vivek@gmail.com>
2026-02-09 18:06:36 +0530

    Added User Models

--- DB.go
+++ DB.go
@@ -1,44 +1,55 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "gorm.io/driver/sqlite"
-    "gorm.io/gorm"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Note struct {
-    ID      uint   `gorm:"primaryKey"`
-    Title   string `gorm:"size:255"`
-    Content string `gorm:"size:1000"`
+	ID      uint   `gorm:"primaryKey"`
+	Title   string `gorm:"size:255"`
+	Content string `gorm:"size:1000"`
 }
 
+type User struct {
+	ID        uint
+	Username  string
+	Password  string
+	FirstName string
+	LastName  string
+}
+
+type UserToNote struct {
+	ID     uint
+	UserId uint
+	NoteId uint
+}
 
 var DB *gorm.DB
 
-
 func InitDB() *gorm.DB {
-    var err error
-    DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect database:", err)
-    }
+	var err error
+	DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect database:", err)
+	}
 
-    err = DB.AutoMigrate(&Note{})
-    if err != nil {
-        log.Fatal("Failed to migrate database:", err)
-    }
+	err = DB.AutoMigrate(&Note{}, &User{}, &UserToNote{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
 
-    log.Println("Database connected and migrated")
-    return DB
+	log.Println("Database connected and migrated")
+	return DB
 }
 
-// CloseDB closes the connection
 func CloseDB(db *gorm.DB) {
-    sqlDB, err := db.DB()
-    if err != nil {
-        log.Println("Failed to get SQL DB:", err)
-        return
-    }
-    sqlDB.Close()
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Println("Failed to get SQL DB:", err)
+		return
+	}
+	sqlDB.Close()
 }
